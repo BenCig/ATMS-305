cb7e5aa2970069c561cb5b789273df2a7811d854
Cigelnik <cigelni2@keeling-f01.(none)>
2017-02-10 14:47:48 -0600

    updated

--- Week4Answers/hw3.1script.py
+++ Week4Answers/hw3.1script.py
@@ -1,14 +1,18 @@
 cars = 100
 drivers = 30
-passengers = 85
+passengers = 85.0
 
 leftcars = cars - drivers
 usedcars = drivers
 totaltransport = drivers + passengers
-averagepass = totaltransport / usedcars
+averagepass = passengers / usedcars
 
-print(leftcars, 'cars will not be used on this day.')
-print(usedcars, 'cars will be used on this day.')
-print(totaltransport, 'total people will be transported on this day.')
-print('The average passenger per car is ', averagepass)
+print(leftcars)
+print( "cars will not be used on this day.")
+print(usedcars) 
+print('cars will be used on this day.')
+print(totaltransport)
+print( 'total people will be transported on this day.')
+print('The average passenger per car is, ')
+print(averagepass)
 
