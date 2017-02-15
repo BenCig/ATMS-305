826f2ffc3e39716dbdc9d83fbde66cb81ee83eaf
Cigelnik <cigelni2@keeling.earth.illinois.edu>
2017-02-15 10:31:08 -0600

    updated

--- Week5Answers/hw4.1script.py
+++ Week5Answers/hw4.1script.py
@@ -1,15 +1,13 @@
 filename='aravg.ann.land_ocean.90S.90N.v4.0.1.201612.asc'
-year = []
-temperature = []
-
-with open(filename, "r") as f:
+wxdata={'year':[],'temperature':[]}
+with open(filename, "r") as f: #read all the lines in the file
     alist = f.read().splitlines()
-for line in alist:
-    year.append(int(line.split()[0]))
-    temperature.append(float(line.split()[1]))
-
-wxdata = list(zip(year,temperature))
-wxdata.sort(reverse=True)
+for line in alist: #iterate through lines
+    wxdata['year'].append(int(line.split()[0]))
+    wxdata['temperature'].append(float(line.split()[1]))
 
 print('Ranking	'+'Year	'+'Temperature Anomaly (degrees C)	'\
 +'Temperature Anomaly (degrees F)')
+
+# try two lists, zip them, sort, shove into dictionary???
+
--- Week5Answers/hw4.2script.py
+++ Week5Answers/hw4.2script.py
@@ -1,5 +1,36 @@
-# do a couple diff lists or do a dictionary?
+#do a couple diff  lists or do a dictionary?
 # then count(-SN), count(SN), count(+SN)
 # 2. divide that by total number
 # 3. find the high
 # 4. oh fun...
+
+filename='CMI.csv'
+wxdata={'obs':[], 'temperature':[]}
+
+with open (filename, "r") as f:
+    first_line = f.readline()
+    alist = f.read().splitlines()
+for line in alist:
+     wxdata['temperature'].append(line.split(',')[2])
+     wxdata['obs'].append(line.split(',')[10])
+
+# count up all types of snow (done like this bc count('SN') gave incorrect
+lightsnow = int(wxdata['obs'].count('-SN'))
+brlsnow = int(wxdata['obs'].count('-SN BR'))
+bllsnow = int(wxdata['obs'].count('-SN BLSN'))
+frzlsnow = int(wxdata['obs'].count('-SN FZFG'))
+blsnow = int(wxdata['obs'].count('SN BLSN'))
+fgsnow = int(wxdata['obs'].count('SN FG'))
+fzsnow = int(wxdata['obs'].count('SN FZFG'))
+brsnow = int(wxdata['obs'].count('SN BR'))
+heavysnow = int(wxdata['obs'].count('+SN FG'))
+totalsnow = lightsnow + brlsnow + bllsnow + frzlsnow + blsnow + fgsnow +\
+    fzsnow + brsnow + heavysnow
+
+totalobs = (len(wxdata['obs']))
+print(totalobs)
+
+# total snow for # of snow obs
+# total snow / total obs for fraction of reports
+
+
