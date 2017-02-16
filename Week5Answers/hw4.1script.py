8de0d492524e5ad17e41814f6cd3224a67b382a4
Cigelnik <cigelni2@keeling.earth.illinois.edu>
2017-02-15 21:52:33 -0600

    updated

--- Week5Answers/hw4.1script.py
+++ Week5Answers/hw4.1script.py
@@ -1,13 +1,22 @@
 filename='aravg.ann.land_ocean.90S.90N.v4.0.1.201612.asc'
 wxdata={'year':[],'temperature':[]}
-with open(filename, "r") as f: #read all the lines in the file
+with open(filename, "r") as f: 
     alist = f.read().splitlines()
 for line in alist: #iterate through lines
     wxdata['year'].append(int(line.split()[0]))
     wxdata['temperature'].append(float(line.split()[1]))
 
-print('Ranking	'+'Year	'+'Temperature Anomaly (degrees C)	'\
+print('Ranking      '+'Year     '+'Temperature Anomaly (degrees C)     '\
 +'Temperature Anomaly (degrees F)')
 
-# try two lists, zip them, sort, shove into dictionary???
+highesttemps = [i for i in wxdata['temperature'] if i >= .3]
+highesttemps.sort(reverse = True)
 
+# convert temp anomaly into F
+
+for i in range (0,10):
+    rank = i + 1
+    tempF = highesttemps[i] * 1.8 + 32
+    print('{0:<12d} {1:<8d} {2:<35f} {3:<10f} '.format(rank,\
+    wxdata['year'][wxdata['temperature'].index(highesttemps[i])]\
+    ,highesttemps[i], tempF))
--- Week5Answers/hw4.2script.py
+++ Week5Answers/hw4.2script.py
@@ -1,36 +1,48 @@
-#do a couple diff  lists or do a dictionary?
-# then count(-SN), count(SN), count(+SN)
-# 2. divide that by total number
-# 3. find the high
-# 4. oh fun...
-
 filename='CMI.csv'
-wxdata={'obs':[], 'temperature':[]}
-
+day = []
+obs = []
+temp = []
 with open (filename, "r") as f:
     first_line = f.readline()
     alist = f.read().splitlines()
 for line in alist:
-     wxdata['temperature'].append(line.split(',')[2])
-     wxdata['obs'].append(line.split(',')[10])
+    obs.append(line.split(',')[10])
+    day.append(line.split(',')[1])
+    temp.append(line.split(',')[2])
+
+day.remove('2016-12-20 11:53')
+day.remove('2016-05-01 00:00')
+temp.remove('M')
+temp.remove('M')
+
+floattemp = [float(i) for i in temp]
+tempsum = sum(floattemp)
+average = tempsum / (len(day))
+
+wxdata = dict(zip(floattemp[:],day[:]))
+
+hightemp = [i for i in wxdata if i >= 90]
+hightemp.sort(reverse = True)
 
 # count up all types of snow (done like this bc count('SN') gave incorrect
-lightsnow = int(wxdata['obs'].count('-SN'))
-brlsnow = int(wxdata['obs'].count('-SN BR'))
-bllsnow = int(wxdata['obs'].count('-SN BLSN'))
-frzlsnow = int(wxdata['obs'].count('-SN FZFG'))
-blsnow = int(wxdata['obs'].count('SN BLSN'))
-fgsnow = int(wxdata['obs'].count('SN FG'))
-fzsnow = int(wxdata['obs'].count('SN FZFG'))
-brsnow = int(wxdata['obs'].count('SN BR'))
-heavysnow = int(wxdata['obs'].count('+SN FG'))
-totalsnow = lightsnow + brlsnow + bllsnow + frzlsnow + blsnow + fgsnow +\
-    fzsnow + brsnow + heavysnow
+lightsnow = int(obs.count('-SN'))
+brlsnow = int(obs.count('-SN BR'))
+bllsnow = int(obs.count('-SN BLSN'))
+frzlsnow = int(obs.count('-SN FZFG'))
+blsnow = int(obs.count('SN BLSN'))
+fgsnow = int(obs.count('SN FG'))
+fzsnow = int(obs.count('SN FZFG'))
+brsnow = int(obs.count('SN BR'))
+heavysnow = int(obs.count('+SN FG'))
+totalsnow = lightsnow + brlsnow + bllsnow + frzlsnow + blsnow + fgsnow + fzsnow+\
+    brsnow + heavysnow
 
-totalobs = (len(wxdata['obs']))
-print(totalobs)
+totalobs = (len(obs))
+snowfrac = float(totalsnow/totalobs)
 
-# total snow for # of snow obs
-# total snow / total obs for fraction of reports
-
-
+print("1. There were",totalsnow,"snow reports last year.")
+print("2. The fraction of reports with snow was:", snowfrac)
+print("3. The highest temperature of", hightemp[0], "occured at"\
+    ,wxdata[hightemp[0]])
+print("4. The mean temperature of all reports was:", \
+    average, "degrees Fahrenheit.")
